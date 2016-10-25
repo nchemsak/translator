9f0368ede4324a3b0897e11f2f7035ccc570f31c
Nick Chemsak <nchemsak@gmail.com>
2016-10-25 11:40:47 -0500

    update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,34 @@
-document.getElementById("translate").addEventListener("click", translate);
+var spanish = document.getElementById("spanish");
+var french = document.getElementById("french");
+var german = document.getElementById("german");
+var italian = document.getElementById("italian");
+var canadian = document.getElementById("canadian");
+var output = document.getElementById("output");
 
-function translate() {
-console.log("translate button clicked ");
-  output.innerHTML = "translate button clicked";
-}
+function pushPhrase(languageString) {
+  // var obj = Leet.getTranslation();
+
+    // console.log("obj: ", obj );
+  if (spanish.selected) {
+    console.log("Spanish");
+    var obj = Leet.getTranslation("spanish");
+    output.innerHTML = `${obj.Sombody + obj.besides + obj.Ben + obj.please + obj.answer}`
+  } else if (french.selected){
+    console.log("French");
+    output.innerHTML = Leet.getTranslation("french");
+  } else if (italian.selected) {
+    console.log("Italian");
+  } else if (german.selected) {
+    console.log("German");
+  } else if (canadian.selected) {
+    console.log("Canadian");
+  } else {
+    console.log("error");
+  }
+  }
+
+// button
+document.getElementById("translate").addEventListener("click", pushPhrase);
+
 
 
